ff07f096b68e59da451b8c764ebd4fa6444f251c
Luís Oliveira <luispaisalves2012@gmail.com>
2025-03-01 15:03:54 +0000

    update work experience

--- src/components/ProfissionalExperience.jsx
+++ src/components/ProfissionalExperience.jsx
@@ -1,4 +1,12 @@
-const skills = ["NodeJS", "TypeScript", "JestJS", "RxJS", "Gherkin", "Jenkins"];
+const skills_nodejs = [
+  "NodeJS",
+  "TypeScript",
+  "JestJS",
+  "RxJS",
+  "Gherkin",
+  "Jenkins",
+];
+const skills_android = ["Android", "Kotlin", "C++", "BDD", "AIDL"];
 
 function ProfissionalExperience() {
   return (
@@ -11,9 +19,52 @@ function ProfissionalExperience() {
       <div>
         <div class="flex justify-between">
           <span class="text-md md:text-xl font-bold">
-            Software Engineer @ Critical Software
+            Android Platform Software Engineer
           </span>
-          <span class="text-sm md:text-xl text-gray-400">2021-Present</span>
+          <span class="text-sm md:text-xl text-gray-400">2024-Present</span>
+        </div>
+        <div class="flex justify-between">
+          <span class="text-md md:text-lg font-bold text-gray-400">
+            @ Critical Software
+          </span>
+        </div>
+        <div class="pt-4">
+          <span class="text-md md:text-lg text-gray-400 text-justify">
+            <ul>
+              <li>
+                Designed and developed key components of the car audio system,
+                focusing on optimising the Hardware Abstraction Layer (HAL) in
+                C++ for seamless hardware integration. I also built Android
+                Services in Kotlin to provide audio control and volume
+                management APIs, ensuring a smooth interface between hardware
+                functionality and user-facing applications. These contributions
+                were pivotal in enhancing automotive infotainment systems'
+                functionality and user experience.
+              </li>
+            </ul>
+          </span>
+        </div>
+        <div class="text-md md:text-lg text-center flex flex-wrap pt-4">
+          {skills_android.map((skills) => {
+            return (
+              <div class="mb-2 mr-2 rounded-lg border border-gray-300 bg-gray-100">
+                <span class="m-2">{skills}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div class="mt-4">
+        <div class="flex justify-between">
+          <span class="text-md md:text-xl font-bold">
+            NodeJS Software Engineer
+          </span>
+          <span class="text-sm md:text-xl text-gray-400">2021-2024</span>
+        </div>
+        <div class="flex justify-between">
+          <span class="text-md md:text-lg font-bold text-gray-400">
+            @ Critical Software
+          </span>
         </div>
         <div class="pt-4">
           <span class="text-md md:text-lg text-gray-400 text-justify">
@@ -38,7 +89,7 @@ function ProfissionalExperience() {
           </span>
         </div>
         <div class="text-md md:text-lg text-center flex flex-wrap pt-4">
-          {skills.map((skills) => {
+          {skills_nodejs.map((skills) => {
             return (
               <div class="mb-2 mr-2 rounded-lg border border-gray-300 bg-gray-100">
                 <span class="m-2">{skills}</span>
